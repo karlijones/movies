d847c9d734191b981ad96ba9a21e65fe43d0026b
karlijones <karlijones1984@gmail.com>
2024-09-13 23:06:18 -0500

    Test data added to widget

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,5 +1,12 @@
 it('Shows an autocomplete', () => {
     createAutoComplete({
-        root: document.querySelector('#target')
+        root: document.querySelector('#target'),
+        fetchData() {
+            return[
+                { Title: 'Avengers' },
+                { Title: 'Not Avengers' },
+                { Title: 'Some Other Movie' },
+            ];
+        }
     });
 });
